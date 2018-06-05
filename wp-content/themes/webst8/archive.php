74b724bf2ce88952ec8dc9de63a5c782bc2f1c84
Gento <a551.return@gmail.com>
2018-06-05 23:22:13 +0900

    perfect archive

--- wp-content/themes/webst8/archive.php
+++ wp-content/themes/webst8/archive.php
@@ -1,36 +1,60 @@
 <?php get_header(); ?>
 <!-- 上記が追記するコード -->
- 
- <section id="content">
-	 <div id="content-wrap" class="container">
- 		<div id="main" class="col-md-9">
-<h1>
-    <?php $cat = get_the_category(); ?>
-    <?php $cat = $cat[0]; ?>
-    <?php echo get_cat_name($cat->term_id); ?>
-</h1>
-      <hr>
+       
+<div class="container">
+    <div class="contents">
+        <h1>
+            <?php $cat = get_the_category(); ?>
+            <?php $cat = $cat[0]; ?>
+            <?php echo get_cat_name($cat->term_id); ?>
+        </h1>
+
         <!-- ■■■■■■ 記事を古い順で表示する為のコード ■■■■■■■ -->
         <?php query_posts($query_string .'&order=ASC'); ?>
-      <?php  if ( have_posts() ) :  while ( have_posts() ) : the_post(); ?>
-          <h2>
-            <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
-          </h2>
-          <section>
-            <p>作成日時：<?php the_time('Y年n月j日'); ?></p>
-            <a href="<?php echo get_permalink(); ?>"><?php the_excerpt(); ?></a>
-          </section>
-          <hr>
-      <?php 
+        <?php  if ( have_posts() ) :  while ( have_posts() ) : the_post(); ?>
+        <article <?php post_class(); ?>>
+            <h3>
+                <a href="<?php echo get_permalink(); ?>">
+                    <?php the_title(); ?>
+                </a>
+            </h3>
+
+            <div class="postinfo">
+                <time datetime="<?php echo get_the_date( 'Y-m-d' ) ?>">
+             <i class="fa fa-clock-o"></i>
+      	   <?php echo get_the_date(); ?>
+        </time>
+                <span class="postcat">
+      	   <?php the_category( ', ' ); ?>
+      	    </span>
+            </div>
+
+            <div class="row">
+                <div class="col one-second excerpt">
+                    <?php the_excerpt(); ?>
+                    <p class="more"><a href="<?php the_permalink(); ?>">続きを読む <i class="fa fa-chevron-right"></i></a></p>
+                </div>
+            </div>
+        </article>
+
+        <?php 
           endwhile;
       endif;
       ?>
- 		</div>
- 		<div id="sidebar" class="col-md-3">
-			<?php get_sidebar(); ?>
-			<!-- 上記が追記するコード -->
- 		</div>
- 	</div>
- </section>
- <!-- 下記が追記するコード -->
+
+        <div id="sidebar" class="col-md-3">
+            <?php get_sidebar(); ?>
+            <!-- 上記が追記するコード -->
+        </div>
+    </div>
+    
+    <div class="blogmenu">
+            <ul>
+                <!-- ウィジェット php dynamic_sidebar()を使って、WordPress管理画面の「外観」→「ウィジェット」で設定したサイドバーを表示-->
+                <?php dynamic_sidebar(); ?>
+            </ul>
+        </div>
+        
+</div>
+
 <?php get_footer(); ?>
