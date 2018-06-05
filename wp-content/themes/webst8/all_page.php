07e22063593b4a4e217966813317068a01e72209
Gento <a551.return@gmail.com>
2018-06-05 22:06:28 +0900

    それっぽくした目次。

--- wp-content/themes/webst8/all_page.php
+++ wp-content/themes/webst8/all_page.php
@@ -4,52 +4,29 @@
 ?>
 
 <?php get_header(); ?>
-   
- <?php
-$paged = (int) get_query_var('paged');
+
+<?php 
 $args = array(
-	'paged' => $paged,
-	'orderby' => 'post_date',
-	'order' => 'DESC',
-	'post_type' => 'post',
-	'post_status' => 'publish'
+    'post_type' => 'post', //投稿を表示
+    'posts_per_page' => 100, //表示する件数
 );
-$the_query = new WP_Query($args);
+$the_query = new WP_Query( $args );
 if ( $the_query->have_posts() ) :
-	while ( $the_query->have_posts() ) : $the_query->the_post();
 ?>
-	
-	
+
  <div class="container">
-        <div class="contents">
-<h3>    
-    <a href="<?php the_permalink(); ?>">
-        <?php the_title(); ?>
-    </a>
-</h3>
-           
+<h2>目次</h2>
+    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+             <div class="col-md-6 col-md-offset-3">
+              <article <?php post_class(); ?>>
+              <h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
+                 </article>
      </div>
+    <?php endwhile; ?>
+ 
+<?php endif; ?>
+     
 </div>
-                
-                
-                
-                
-<?php endwhile; endif; ?>
- 
-<?php
-if ($the_query->max_num_pages > 1) {
-	echo paginate_links(array(
-		'base' => get_pagenum_link(1) . '%_%',
-		'format' => 'page/%#%/',
-		'current' => max(1, $paged),
-		'total' => $the_query->max_num_pages
-	));
-}
-?>
- 
-<?php wp_reset_postdata(); ?>
-  
-  
   
   
   
